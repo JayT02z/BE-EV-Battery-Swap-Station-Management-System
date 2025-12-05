b6b089fbd261cc0602c586a04c9cd677af1a6b35
Newbie <130166278+Newbie1402@users.noreply.github.com>
2025-12-05 23:33:03 +0700

    Change test web environment to RANDOM_PORT

--- api-gateway/src/test/java/com/boilerplate/code/GatewayApplicationTests.java
+++ api-gateway/src/test/java/com/boilerplate/code/GatewayApplicationTests.java
@@ -5,15 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = GatewayApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(classes = GatewayApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {
-		"spring.autoconfigure.exclude=" +
-				"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-				"org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-				"org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration," +
-				"org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration," +
-				"org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration",
-		"spring.main.allow-bean-definition-overriding=true"
+		"eureka.client.enabled=false",
+		"spring.cloud.gateway.discovery.locator.enabled=false",
+		"spring.cloud.discovery.enabled=false",
+		"spring.cloud.gateway.enabled=true"
 })
 class GatewayApplicationTests {
 
