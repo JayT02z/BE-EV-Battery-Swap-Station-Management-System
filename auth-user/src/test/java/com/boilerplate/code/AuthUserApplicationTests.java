04568aaebc4a28409170f18e97b0d92b5c8cfb0f
Newbie <130166278+Newbie1402@users.noreply.github.com>
2025-12-05 23:33:55 +0700

    Change test environment to RANDOM_PORT

--- auth-user/src/test/java/com/boilerplate/code/AuthUserApplicationTests.java
+++ auth-user/src/test/java/com/boilerplate/code/AuthUserApplicationTests.java
@@ -5,14 +5,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(classes = AuthUserApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(classes = AuthUserApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {
-		"spring.autoconfigure.exclude=" +
-				"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-				"org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-				"org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration," +
-				"org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration," +
-				"org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration",
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.flyway.enabled=false",
+		"eureka.client.enabled=false",
+		"spring.cloud.discovery.enabled=false",
+		"app.kafka.enabled=false",
+		"spring.mail.host=localhost",
+		"spring.mail.port=25",
 		"spring.main.allow-bean-definition-overriding=true"
 })
 class AuthUserApplicationTests {
